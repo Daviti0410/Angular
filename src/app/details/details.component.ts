e6f04a05282e9ebc6c86acbd3891869d40797c1b
davitlazariahvili <davit.lazariashvili@leverx.com>
2025-06-01 19:46:43 +0400

    feat: covered Dynamic routes and services and apllied it to project

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {HousingLocation} from '../housing-location';
+import {HousingService} from '../housing.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-details',
@@ -7,5 +10,12 @@ import { Component } from '@angular/core';
   styleUrl: './details.component.scss'
 })
 export class DetailsComponent {
+housingService: HousingService = inject(HousingService);
+housingLocation!: HousingLocation;
+route: ActivatedRoute = inject(ActivatedRoute);
 
+constructor() {
+  const housingLocationId = Number(this.route.snapshot.paramMap.get('id'));
+  this.housingLocation = this.housingService.getHousingLocationById(housingLocationId)!;
+}
 }
